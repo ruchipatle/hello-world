943414b6633a94fa20f6546791f6dcf9da744d0b
Ruchi Patle <ruchi.patle@hp.com>
2014-07-08 14:27:09 +0530

    added Property

---
+++ Property.java
@@ -0,0 +1,23 @@
+public class Property {
+	private String name;
+	private String value;
+	
+	public Property(String property, String value){
+		this.name = property;
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
